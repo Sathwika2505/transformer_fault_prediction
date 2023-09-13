4655b065855dcd9b502a49ec6ee3ade74b1a791d
SathwikaGopishetty <111847859+SathwikaGopishetty@users.noreply.github.com>
2023-09-13 19:44:09 +0530

    asdfghjkl;

---
+++ feature_engineering.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+from data_preprocessing import data_preprocess
+
+def feature_engineering():
+
+    data = data_preprocess()
+    # print(data)
+    count_0, count_1 = data['MOG_A'].value_counts()
+    class_0 = data[data['MOG_A'] == 0]
+    class_1 = data[data['MOG_A'] == 1]
+    Target_1_over = class_1.sample(count_0,replace=True)
+    # print(Target_1_over)
+    dataset_balanced = pd.concat([Target_1_over,class_0], axis=0)
+    # print(dataset_balanced)
+
+    data = dataset_balanced.copy()
+    return data
+
+feature_engineering()
